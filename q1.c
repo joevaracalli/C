c9f8b6cb76e7a42f1d4bd60f2dc0d98ed20f288e
Joseph Varacalli <58127285+joevaracalli@users.noreply.github.com>
2020-05-14 21:21:12 -0400

    Add files via upload

---
+++ q1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+  // define an array to store the input in the array
+  int store[3];
+
+  // a loop asking for the input of 5 ints and storing them in the array
+  for(int input = 0; input < 5; input++)
+  {
+    // ask user for input
+    printf("Enter a number:\n");
+    // stores input
+    scanf("%d", &store[input]);
+  }
+
+  // creates a new line for more space
+  printf("\n");
+
+  // a loop for the out of the first 3 ints in the array
+  for(int output = 0; output < 3; output++)
+  {
+    // outputs a int
+    printf("%d\n", store[output]);
+  }
+
+  return 0;
+}
