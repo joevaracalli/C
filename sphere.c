4a47bbdf2dd7fd9465cf02549c4bd545feec5856
Joseph Varacalli <58127285+joevaracalli@users.noreply.github.com>
2021-05-17 20:59:06 -0400

    Add files via upload

---
+++ sphere.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define Pi 3.14252
+
+int main(void)
+{
+   float volume, radius;
+   printf("Enter the radius: "");
+   scanf("%f", &radius);
+   volume = (4.0f/3.0f*Pi*radius*radius*radius);
+   printf("The volume is: %0.2f\n" , volume);
+   return 0;
+}
